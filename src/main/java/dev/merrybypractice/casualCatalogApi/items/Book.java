1238e1818ebe555c664a8ed21375bcfd08ef594c
Merry Cimakasky <mkcimakasky@gmail.com>
2019-06-25 20:48:43 -0700

    more on item

--- src/main/java/dev/merrybypractice/casualCatalogApi/items/Book.java
+++ src/main/java/dev/merrybypractice/casualCatalogApi/items/Book.java
@@ -1,10 +1,24 @@
 package dev.merrybypractice.casualCatalogApi.items;
 
-import javax.persistence.Entity;
+import java.util.Date;
+import java.util.List;
 
-@Entity
-public class Book extends Item{
+public class Book extends Item {
 
+    //needs title property
+    public String title;
+    // needs gift boolean
+    public boolean gift;
+    // needs current location
+    public String location = null;
+    //needs creator property - list *
+    public List<String> creator = null;
+    // needs acquired date *
+    public Date aquiredDate = null;
+    // needs acquired location *
+    public String aquiredLocation = null;
+    // needs acquired price *
+    public Long aquiredPrice = null;
 
 
 }
--- src/main/java/dev/merrybypractice/casualCatalogApi/items/Item.java
+++ src/main/java/dev/merrybypractice/casualCatalogApi/items/Item.java
@@ -6,24 +6,101 @@ import java.util.List;
 
 @Entity
 public class Item {
-    //base item in a personal collection
-        //needs title property
-        public String title;
-        // needs gift boolean
-        public boolean gift;
-        // needs current location
-        public String location = null;
-        //needs creator property - list *
-        public List<String> creator = null;
-        // needs acquired date *
-        public Date aquiredDate = null;
-        // needs acquired location *
-        public String aquiredLocation = null;
-        // needs acquired price *
-        public Long aquiredPrice = null;
+    //needs title property
+    private String title;
 
+    //title getter
+    public String getTitle() {
+        return title;
+    }
 
+    //title setter
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    // needs gift boolean
+    private boolean gift;
+
+    //gift getter
+    public boolean isGift() {
+        return gift;
+    }
+
+    //gift setter
+    public void setGift(boolean gift) {
+        this.gift = gift;
+    }
+
+    // needs current location
+    private String location = null;
+
+    //location getter
+    public String getLocation() {
+        return location;
+    }
+
+    //location setter
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    //needs creator property - list *
+    private List<String> creator = null;
+
+    public List<String> getCreator() {
+        return creator;
+    }
+
+    public void setCreator(List<String> creator) {
+        this.creator = creator;
+    }
+
+    // needs acquired date *
+    private Date aquiredDate = null;
+
+    //acquired date getter
+
+    public Date getAquiredDate() {
+        return aquiredDate;
+    }
+
+    //acquired date setter
+
+    public void setAquiredDate(Date aquiredDate) {
+        this.aquiredDate = aquiredDate;
+    }
+
+    // needs acquired location *
+    private String aquiredLocation = null;
+
+    //get Acquired Location
+
+    public String getAquiredLocation() {
+        return aquiredLocation;
+    }
+
+    //set Acquired Location
+
+    public void setAquiredLocation(String aquiredLocation) {
+        this.aquiredLocation = aquiredLocation;
+    }
+
+    // needs acquired price *
+    private Long aquiredPrice = null;
+
+    //get acquired price
+
+    public Long getAquiredPrice() {
+        return aquiredPrice;
+    }
+
+    //set acquired price
+
+    public void setAquiredPrice(Long aquiredPrice) {
+        this.aquiredPrice = aquiredPrice;
+    }
 
     //possible options:
-        // place in a series?
+    // place in a series?
 }
