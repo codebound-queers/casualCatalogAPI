240b1379f30039b991331e058fa765d35b3b7f22
Merry Cimakasky <mkcimakasky@gmail.com>
2019-06-25 19:53:22 -0700

    first init

---
+++ src/main/java/dev/merrybypractice/casualCatalogApi/CasualCatalogApiApplication.java
@@ -0,0 +1,13 @@
+package dev.merrybypractice.casualCatalogApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasualCatalogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasualCatalogApiApplication.class, args);
+	}
+
+}
